e199df7c767f83c1c52913ea8289d7c7d6b17f41
andyt <tranandy@oregonstate.edu>
2019-07-21 22:28:41 -0500

    check unittest1 compilation

---
+++ projects/tranandy/dominion/unittest1.c
@@ -0,0 +1,26 @@
+// unit test for ambassador card
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int main(){
+	int i;
+    int seed = 1000;
+    int numPlayer = 2;
+    int maxBonus = 10;
+    int p, r, handCount;
+    int bonus;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+    int maxHandCount = 5;
+
+	r = initializeGame(numPlayer, k, seed, &G);
+	G.handCount[p] = handCount;
+
+	return 0;
+}
